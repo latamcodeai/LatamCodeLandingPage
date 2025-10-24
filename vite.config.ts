6a018ecf0252136afcc8a66f65551879ac553752
Federico Tomadin <admin@latamcodeai.com>
2025-10-24 14:01:19 -0300

    fix: update HTML structure, manifest details, and Vite configuration for improved app setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/', // Important for production
     server: {
         port: 3000
+    },
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        sourcemap: false
     }
 })
