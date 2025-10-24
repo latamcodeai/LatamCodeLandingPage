a9dca719fbc1c84990e4a747fa457743ae4db8af
Federico Tomadin <admin@latamcodeai.com>
2025-10-24 16:24:20 -0300

    feat: set up GitHub Pages deployment workflow and refine Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/', // Important for production
+    base: '/',
     server: {
         port: 3000
     },
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
-        sourcemap: false
     }
 })
